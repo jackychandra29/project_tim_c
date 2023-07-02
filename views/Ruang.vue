302821301457b23f2bf5c0bf6bdd45e18e230bf8
xcodestitcher <90361315+xcodestitcher@users.noreply.github.com>
2023-07-02 20:00:22 +0700

    Add files via upload
    part 1: 
    - tombol tambah data tiap menu udah dibuat
    - tambah data siswa selesai (termasuk penghapusan form id)
    - tiap halaman tambah data udah dihubungkan ke routes

---
+++ views/Ruang.vue
@@ -0,0 +1,182 @@
+<template>
+  <Header></Header>
+  <Sidebar></Sidebar>
+  <main id="main" class="main">
+    <div class="pagetitle">
+      <h1 style="text-align: left;">Data Sebaran Ruang</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/ruang">Ruang</a></li>
+
+        </ol>
+      </nav>
+    </div>
+    <!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="card">
+            <div class="card-body">
+              <!-- Table with stripped rows -->
+              <div
+                class="datatable-wrapper datatable-loading no-footer sortable searchable fixed-columns"
+              >
+                <div class="datatable-top">
+                  <div class="datatable-dropdown">
+                    <label>
+                      <select class="datatable-selector">
+                        <option value="5">5</option>
+                        <option value="10" selected="">10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                        <option value="25">25</option>
+                      </select>
+                      entries per page
+                    </label>
+                  </div>
+                  <div class="datatable-search">
+                    <input
+                      class="datatable-input"
+                      placeholder="Search..."
+                      type="search"
+                      title="Search within table"
+                      style="margin-right: 10px;"
+                    />
+                    <router-link :to="'/tambahruang'" class="btn btn-primary rounded-pill">Tambah Data</router-link>
+                  </div>
+                </div>
+                <div class="datatable-container">
+                  <table class="table datatable datatable-table">
+                    <thead>
+                      <tr>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#" class="datatable-sorter" style="text-align: left;">Kode Ruang</a>
+                        </th>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#" class="datatable-sorter" style="text-align: left;">Nama Ruang</a>
+                        </th>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#" class="datatable-sorter" style="text-align: left;">Panjang</a>
+                        </th>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#" class="datatable-sorter" style="text-align: left;">Lebar</a>
+                        </th>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#" class="datatable-sorter" style="text-align: left;">Lantai</a>
+                        </th>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#" class="datatable-sorter" style="text-align: left;">Kode Bangunan</a>
+                        </th>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#" class="datatable-sorter" style="text-align: left;">Kode Jenis Ruang</a>
+                        </th>
+                        <th
+                          data-sortable="true"
+                        >
+                          <a href="#">Aksi</a>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="(rg, Kode_ruang) in ruangs.slice(0, 5)" :key="Kode_ruang"
+                      >
+                        <td style="text-align: left;">{{ rg.Kode_ruang }}</td>
+                        <td style="text-align: left;">{{ rg.Nama_ruang }}</td>
+                        <td style="text-align: left;">{{ rg.Panjang }}</td>
+                        <td style="text-align: left;">{{ rg.Lebar }}</td>
+                        <td style="text-align: left;">{{ rg.Lantai }}</td>
+                        <td style="text-align: left;">{{ rg.Kode_bangunan }}</td>
+                        <td style="text-align: left;">{{ rg.Kode_jenis_ruang }}</td>
+                        <td><button type="button" class="btn btn-warning rounded-pill">Edit</button></td>
+
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div class="datatable-bottom">
+                  <div class="datatable-info">Showing 1 to 5 of 5 entries</div>
+                  <nav class="datatable-pagination">
+                    <ul class="datatable-pagination-list"></ul>
+                  </nav>
+                </div>
+              </div>
+              <!-- End Table with stripped rows -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <Footer></Footer>
+</template>
+
+<script>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+import Header from '../components/Header.vue'
+import Sidebar from '../components/Sidebar.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Sidebar,
+    Footer,
+  },
+  setup() {
+    //reactive state
+    let ruangs = ref([]);
+
+    //mounted
+    onMounted(() => {
+      //get API from Laravel Backend
+      axios
+        .get("http://localhost:8000/api/ruang")
+        .then((response) => {
+          //asign state ruangs with response data
+          ruangs.value = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+    });
+    //method delete
+    function ruangDelete(id) {
+      //delete data ruang by ID
+      axios
+        .delete(`http://localhost:8000/api/ruang/${id}`)
+        .then(() => {
+          //splice ruangs
+          ruangs.value.splice(ruangs.value.indexOf(id), 1);
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+    }
+
+    //return
+    return {
+      ruangs,
+      ruangDelete,
+    };
+  },
+};
+</script>
